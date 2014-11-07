47782ef254e4efde22c6a4ee4a732213db0e3184
cj360 <ayunker551@gmail.com>
2014-11-06 20:38:49 -0500

    Not sure why I never added a pa.dependencies...
    Change-Id: I31b4d52ebaf5b7a044ae5798af0116ef0cce58a7
    Signed-off-by: cj360 <ayunker551@gmail.com>
    
    correct typo
    
    Change-Id: I1dee10dda95cec895a12182de5dddb4e48c8d47b
    Signed-off-by: cj360 <ayunker551@gmail.com>

--- full_moto_msm8960.mk
+++ full_moto_msm8960.mk
@@ -23,8 +23,12 @@
 # lines, full and moto_msm8960, hence its name.
 #
 
+TARGET_SCREEN_WIDTH := 720
+TARGET_SCREEN_HEIGHT := 1280
+
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
 # Inherit from moto_msm8960 device
 $(call inherit-product, device/motorola/moto_msm8960_jbbl/device_moto_msm8960.mk)
 
